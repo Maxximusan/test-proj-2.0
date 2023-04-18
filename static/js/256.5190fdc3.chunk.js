9bf30c39bde38fa4c15a8a80e90b836882c1cf1f
Maxximusan <103362267+Maxximusan@users.noreply.github.com>
2023-04-18 17:32:54 +0000

    Deploying to gh-pages from @ Maxximusan/test-proj-2.0@3907335fdf888a6380536a79dccfe837276432d4 🚀

--- static/js/256.5190fdc3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_proj_2_0=self.webpackChunktest_proj_2_0||[]).push([[256],{256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
-//# sourceMappingURL=256.5190fdc3.chunk.js.map
